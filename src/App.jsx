f75a21482727b9536d9c8fdd5af2bc4e6bd83919
Zoltan Pozsonyi <pozsonyizoli@gmail.com>
2024-07-01 15:58:16 +0200

    updating project with basic project template + env variable handling

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,50 @@
+import { useRef } from "react";
+import { CONFIG } from "./environment";
+
 function App() {
-  return <div className="text-center">Hello world</div>;
+  const dialogRef = useRef(null);
+
+  const openDialog = () => {
+    dialogRef.current.showModal();
+  };
+
+  const closeDialog = () => {
+    dialogRef.current.close();
+  };
+
+  return (
+    <>
+      <div className="flex flex-col h-full w-full">
+        <header className="h-[10%] flex items-center justify-center bg-gradient-to-br from-green-700 to-green-500">
+          <div className="text-center m-auto font-bold text-3xl text-stone-300">Hello world</div>
+        </header>
+        <main className="flex flex-col flex-grow p-2">
+          <div className="flex flex-row h-full">
+            <div className="w-1/5 bg-gray-200">Sidebar</div>
+            <div className="w-4/5 bg-gray-400">{CONFIG}</div>
+          </div>
+        </main>
+
+        <footer className="h-[20px] xl:h-[40px] bg-gray-200 bg-gradient-to-br from-green-700 to-green-500 flex justify-end gap-4 px-2 items-center">
+          <button onClick={openDialog}>About</button>
+          <p className="">Template project | 2024</p>
+        </footer>
+      </div>
+      <dialog ref={dialogRef}>
+        <div className="w-[80vw] h-[80vh] p-4 flex flex-col items-center justify-between">
+          <p className="font-bold text-2xl">Title</p>
+          <div>
+            <p>Line 1</p>
+            <p>Line 2</p>
+            <p>Line 3</p>
+          </div>
+          <button onClick={closeDialog} className="bg-green-600 py-2 px-4 rounded-md text-stone-200">
+            Close
+          </button>
+        </div>
+      </dialog>
+    </>
+  );
 }
 
 export default App;
